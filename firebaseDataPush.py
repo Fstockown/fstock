7eeaaa3ddb582821b6b14d836e6f4acc176ddcfe
aman <fstockown@gmail.com>
2021-06-15 11:56:32 +0530

    update

--- firebaseDataPush.py
+++ firebaseDataPush.py
@@ -1,29 +1,51 @@
 from datetime import datetime
+import json
 import pyrebase
-
+import requests
+from bs4 import BeautifulSoup
 #config data for firebase 
 
 config ={
-    "apiKey": "AIzaSyCY1CKDsoF4frwE9hDv0Cvybc559hUrb70",
-    "authDomain": "fstock-9ced2.firebaseapp.com",
-    "projectId": "fstock-9ced2",
-    "databaseURL": "https://fstock-9ced2-default-rtdb.asia-southeast1.firebasedatabase.app",
-    "storageBucket": "fstock-9ced2.appspot.com",
-    "messagingSenderId": "956197289236",
-    "appId": "1:956197289236:web:a96a757386b39788435601",
-    "measurementId": "G-HKY3F089QX"
+  "apiKey": "AIzaSyCY1CKDsoF4frwE9hDv0Cvybc559hUrb70",
+  "authDomain": "fstock-9ced2.firebaseapp.com",
+  "databaseURL": "https://fstock-9ced2-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "projectId": "fstock-9ced2",
+  "storageBucket": "fstock-9ced2.appspot.com",
+  "messagingSenderId": "956197289236",
+  "appId": "1:956197289236:web:649d4a76569d3341435601",
+  "measurementId": "G-HZ7Z7R2HLG"
 }
-
 # initilizing pyrebase
 firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+# Log the user in
+user = auth.sign_in_with_email_and_password("admin@fstock.com","fstockadmin7987")
+
+#function to convert string into float
+def convertType(data):
+        inte=str()
+        datali = list(data)
+        for i in datali:
+                if i != ",":
+                        inte = inte+i
+        result = float(inte)
+        return result
+
+
+
+def upload_data(data,stock,date):
+    db = firebase.database()
+    db.child("stocks").child(stock).child(date).set(data)
+
 
 #function to make stock datalist 
 def makeStock_data(current,date,stock):
     min = 0 
     max = current
     try:
-        min = firebase.child("stock_data").child(stock).child(date).get().value()["min"]
-        max = firebase.child("stock_data").child(stock).child(date).get().value()["max"]
+        min = int(firebase.child("stock_data").child(stock).child(date).get().value()["min"])
+        max = int(firebase.child("stock_data").child(stock).child(date).get().value()["max"])
     except Exception as e :
         print(e)
     if min > current :
@@ -32,6 +54,7 @@ def makeStock_data(current,date,stock):
         max = current
     data_map = {"date":date, "min":min, "max":max, "current":current}
     return data_map
+
 def get_dataLive(api):
     # function to get data from api
     r = requests.get(api)
@@ -42,14 +65,35 @@ def get_dataLive(api):
     return str(web)
     
 
-api_list = {"stock":"api"}
+api_list = {
+    "sensex":"https://in.finance.yahoo.com/quote/%5EBSESN?p=%5EBSESN",
+    "nifty50":"https://in.finance.yahoo.com/quote/%5ENSEI?p=%5ENSEI"
+    "bitcoin":"https://in.finance.yahoo.com/quote/BTC-INR?p=BTC-INR"
+    "dogecoin":"https://in.finance.yahoo.com/quote/DOGE-INR?p=DOGE-INR"
+    "SBI":"https://in.finance.yahoo.com/quote/SBIN.NS?p=SBIN.NS&.tsrc=fin-srch"
+    "HDFC":"https://in.finance.yahoo.com/quote/HDFCBANK.NS?p=HDFCBANK.NS&.tsrc=fin-srch"
+    "relience":"https://in.finance.yahoo.com/quote/RELIANCE.NS?p=RELIANCE.NS&.tsrc=fin-srch"
+    "tesla,inc":"https://in.finance.yahoo.com/quote/TSLA?p=TSLA&.tsrc=fin-srch"
+    "facebook,inc":"https://in.finance.yahoo.com/quote/FB?p=FB&.tsrc=fin-srch"
+    "amazon,inc":"https://in.finance.yahoo.com/quote/AMZN?p=AMZN&.tsrc=fin-srch"
+    "tata_motors":"https://in.finance.yahoo.com/quote/TTM?p=TTM&.tsrc=fin-srch"
+    "tata_steel_limited":"https://in.finance.yahoo.com/quote/TATASTEEL.NS?p=TATASTEEL.NS&.tsrc=fin-srch"
+}
 
 while True:
-    date = datetime.date(datetime.now())
-    day =  date.weekday()
+    date_forDay = datetime.date(datetime.now())
+    day =  date_forDay.weekday()
+    date = str(date_forDay)
     time = datetime.now().hour
     if day != 5 and day != 6:
         if time >=9 and time <= 15:
+            for i in api_list:
+                stock = i 
+                api = api_list[i]
+                data_str = get_dataLive(api)
+                data = convertType(data_str)
+                data_map = makeStock_data(data, date ,stock)
+                upload_data(data_map, stock,date)
 
             # main database edit here 
 
